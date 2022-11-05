65b158126b7b8159390b0e6eec806aac39262367
IbrahimErkan <76455945+IbrahimErkan@users.noreply.github.com>
2022-11-05 12:50:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-# Lead Calculation with Rule-Based Classification
-
-# BUSINESS PROBLEM:
-# A game company wants to create new customer definitions based on level by using some features of its customers,
-# create segments according to these new customer definitions and estimate how much the new customers can earn on average according to these segments.
-
-# For example:
-# It is desired to determine how much a 25-year-old male user from Turkey, who is an IOS user, can earn on average.
-
-
-# Dataset Story:
-# The Persona.csv dataset contains the prices of the products sold by an international game company and some demographic information of the users who buy these products.
-# The data set consists of records created in each sales transaction. This means that the table is not deduplicated.
-# In other words, a user with certain demographic characteristics may have made more than one purchase.
-
-# Features:
-# PRICE - Customer spend amount
-# SOURCE - The type of device the customer is connecting to
-# SEX - Customer's gender
-# COUNTRY - Customer's country
-# AGE - Customer's age
-
+# LEAD CALCULATİON WİTH RULE-BASED CLASSİFİCATİON
 
 
 import numpy as np
